7937d7121770b22f4768ce9ae6dd1f78f6e57be7
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-02-19 22:31:49 +0100

    Adapt 3dCard on grid part 1

--- src/components/ThreeDCard.js
+++ src/components/ThreeDCard.js
@@ -1,34 +1,34 @@
 "use client";
 
 import React from "react";
+import clsx from "clsx";
 import { CardBody, CardContainer, CardItem } from "./Card";
 
-export function ThreeDCardDemo() {
+export function ThreeDCardDemo({ height = 70, width = 'full' }) {
   return (
-    (<CardContainer className="inter-var">
+    (<CardContainer className='h-64 w-lg' >
       <CardBody
-        className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border"
+        className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl border"
       >
 
-        <CardItem
-          translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white title hidden">
+        <CardItem translateY="50" translateZ="101"
+          className="text-xl font-bold text-red-600 title absolute ">
           Make things float in air
         </CardItem>
 
         <CardItem
           as="p"
-          translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 text hidden">
+          translateY="80" translateZ="101"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 text absolute">
           Hover over this card to unleash the power of CSS perspective
         </CardItem>
 
-        <CardItem translateZ="100" className="w-full mt-4">
+        <CardItem translateZ="100" className="w-full">
           <img
             src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             height="1000"
             width="1000"
-            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            className=" w-full h-60 object-cover rounded-xl group-hover/card:shadow-xl"
             alt="thumbnail" />
         </CardItem>
 
--- src/pages/Makeup.js
+++ src/pages/Makeup.js
@@ -10,43 +10,75 @@ function Makeup() {
         exit={{ x: window.innerWidth }}
       >
         <div className="flex flex-col items-center h-screen mt-20">
-          <ThreeDCardDemo />
           <div className="flex flex-col mx-auto max-w-5xl justify-center items-center mb-20">
-            <h2 class="text-3xl font-semibold tracking-tight sm:text-5xl">Mes Makeup</h2>
+            <h2 className="text-3xl font-semibold tracking-tight sm:text-5xl">Mes Makeup</h2>
           
-            <p class="mt-8 font-medium text-pretty sm:text-xl/8">
+            <p className="mt-8 font-medium text-pretty sm:text-xl/8">
               Anim aute id magna aliqua ad ad non deserunt sunt. 
               Qui irure qui lorem cupidatat commodo. 
               Elit sunt amet fugiat veniam occaecat fugiat.
             </p>
           </div>
 
-          <div className="mx-auto w-[900px] h-[900px]">
-            <div className="grid grid-cols-3 gap-2">
-              <div className="bg-blue-400 h-24"></div>
-              <div className="bg-blue-300 h-24"></div>
-              <div className="bg-blue-400 h-24"></div>
+          <div className="mx-auto  h-auto">
+            <div className="grid grid-cols-3 gap-2 ">
+
+              {/* 1 row */}
+              <div className="h-70">
+                <ThreeDCardDemo />
+              </div>
+              <div className="h-70 ">
+                <ThreeDCardDemo width="10" />
+              </div>
+              <div className="h-70 ">
+                <ThreeDCardDemo />
+              </div>
+
+              {/* 2 row */}
+              <div className="h-70 col-span-2 ">
+                <ThreeDCardDemo />
+              </div>
+              <div className="h-70 ">
+                {/* <ThreeDCardDemo /> */}
+              </div>
+
+              {/* 3 row */}
+              <div className="h-70 ">
+                <ThreeDCardDemo />
+              </div>
+              <div className="h-70 ">
+                <ThreeDCardDemo />
+              </div>
+              <div className="h-70 ">
+                <ThreeDCardDemo />
+              </div>
+
+              {/* 4 row */}
+              <div className="h-70 ">
+                <ThreeDCardDemo />
+              </div>
+              <div className="h-70 col-span-2 ">
+                {/* <ThreeDCardDemo /> */}
+              </div>
+
+              {/* 5 row */}
+              <div className="h-70 ">
+                <ThreeDCardDemo />
+              </div>
+              <div className="h-70 ">
+                <ThreeDCardDemo />
+              </div>
+              <div className="h-70 ">
+                <ThreeDCardDemo />
+              </div>
               
-              <div className="bg-blue-400 h-32 col-span-2"></div>
-              <div className="bg-blue-300 h-32"></div>
-              
-              <div className="bg-blue-400 h-24"></div>
-              <div className="bg-blue-300 h-24"></div>
-              <div className="bg-blue-200 h-24"></div>
-              
-              <div className="bg-blue-200 h-24"></div>
-              <div className="bg-blue-300 h-24"></div>
-              <div className="bg-blue-400 h-24"></div>
-              
-              <div className="bg-blue-300 h-32"></div>
-              <div className="bg-blue-400 h-32 col-span-2"></div>
+            
             </div>
           </div>
         
           <div className="mt-20">
             FOOTER
           </div>
-
         </div>
       </motion.div>
     );
