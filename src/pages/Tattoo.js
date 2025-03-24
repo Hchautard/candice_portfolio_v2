9d0ac369a8b01742d15955ca4ec4ac8155b8c29d
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-03-24 22:27:30 +0100

    Improve Card distribution on the Tattoo page

--- src/pages/Tattoo.js
+++ src/pages/Tattoo.js
@@ -1,25 +1,33 @@
 import "../styles/Tattoo.css"
 import CardDistribution from "./CardDistribution";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Tattoo() {
+  const [showComponent, setShowComponent] = useState(false);
+
   useEffect(() => {
       // Add class to body when component mounts
       document.body.classList.add('tattoo-page');
       
-      // Remove class when component unmounts
+      // Set timeout to load CardDistribution after 2 seconds
+      const timer = setTimeout(() => {
+          setShowComponent(true);
+      }, 500);
+      
+      // Remove class and clear timeout when component unmounts
       return () => {
           document.body.classList.remove('tattoo-page');
+          clearTimeout(timer);
       };
   }, []);
   
   return (
       <div className="Tattoo">
           <div className="container-tattoo">
-              <CardDistribution />
+              {showComponent ? <CardDistribution /> : null}
           </div>
       </div>
   );
 }
   
-  export default Tattoo;
+export default Tattoo;
