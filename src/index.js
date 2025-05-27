d6ed1eb0d08f0d2ce48d59a5329b0c5b9c37bc26
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-05-27 23:01:27 +0200

    change routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Makeup from './pages/Makeup';
+import Tattoo from './pages/Tattoo';
+import Header from './components/Header';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <div>Page not found</div>,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '/makeup',
+    element: <Makeup />,
+  },
+  {
+    path: '/tattoo',
+    element: <Tattoo />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
