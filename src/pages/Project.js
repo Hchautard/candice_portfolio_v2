168bf603b7e412c3a8ab7a04657a7b11e90da0cc
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-08-09 22:28:42 +0200

    Modification de la page Shop. Ajout de l'audio de la vidéo pour la transcription.

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -5,12 +5,25 @@ import '../styles/Project.css';
 
 function Project() {
     const [showComponent, setShowComponent] = useState(false);
-    const [videoLoaded, setVideoLoaded] = useState(false);
 
     useEffect(() => {
         document.body.classList.add('project-page');
 
-        if (videoLoaded) {
+        // CHeck if the video file exists
+        const checkVideoExists = async () => {
+            try {
+                const response = await fetch(vid_project, { method: 'HEAD' });
+                if (response.ok) {
+                    setShowComponent(true);
+                } else {
+                    console.error("Video file not found:", vid_project);
+                }
+            } catch (error) {
+                console.error("Error checking video file:", error);
+            }
+        };
+
+        if (checkVideoExists) {
             const timer = setTimeout(() => {
                 setShowComponent(true);
             }, 500);
@@ -22,11 +35,26 @@ function Project() {
         return () => {
             document.body.classList.remove('project-page');
         };
-    }, [videoLoaded]);
+    }, []);
 
     return (
-        <div className="video-container">
-            <Video src={vid_project} />
+        <div className="project-container">
+            <div className="banner">
+                <div className="project-content-container">
+                    <h2 className="tracking-tight">Bienvenue dans mon (futur) shop</h2>
+                    <p className="project-text text-pretty">
+                        Entre deux gorgées de café artisanal, découvrez nos créations textile uniques ou offrez-vous le tatouage de vos rêves.
+                        Notre boutique hybride cultive l'art de l'étrange et du merveilleux, où chaque visite devient une aventure au pays des possibles.
+                    </p>
+                </div>
+            </div>
+            {showComponent && (
+                <div className="video-container">
+                    <Video
+                        src={vid_project}
+                    />
+                </div>
+            )}
         </div>
     );
 }
