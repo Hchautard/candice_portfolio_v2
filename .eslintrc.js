368d4a21278ce7483707f7a6ed2a5396c2be42c4
Hugo <hugo.chautard@etu.univ-amu.fr>
2026-02-09 23:39:34 +0100

    Add ESLint configuration and update package.json for linting support

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  // Environnements (navigateur, node, etc.)
+  env: {
+    "browser": true,
+    "es2021": true,
+    "node": true,
+  },
+
+  parserOptions:  {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+  },
+
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
+  ],
+
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
